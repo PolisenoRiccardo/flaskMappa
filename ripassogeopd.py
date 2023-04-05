05e3ae9d757e2adf93ec240cc9f1cb36187cf555
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2023-04-05 12:46:01 +0000

    pre ripasso

---
+++ ripassogeopd.py
@@ -0,0 +1,7 @@
+import geopandas
+import contextily 
+import matplotlib.pyplot as plt
+
+quartieri = read_file('Datasets/Quartieri/NIL_WM.shp')
+fontanelle = read_file('Datasets/Fontanelle/Fontanelle_OSM_ODbL.shp')
+
